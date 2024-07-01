c0a655190160703c7db2033963407f5861177d13
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-07-01 12:02:08 +0200

    Add Todo-Items and Todo-List

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("Hello from JS");
+class TodoItem {
+
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    displayInfo() {
+        console.log(`Title: ${this.title}`);
+        console.log(`Description: ${this.description}`);
+        console.log(`Due Date: ${this.dueDate}`);
+        console.log(`Priority: ${this.priority}`);
+    }
+}
+
+
+class TodoList {
+
+    constructor(title, todos) {
+        this.title = title;
+        this.todos = [];
+    }
+
+    addTodo(todoItem) {
+        this.todos.push(todoItem)
+    }
+
+    displayTodos() {
+        this.todos.forEach((todo, index) => {
+            console.log(`Todo ${index + 1}:`);
+            todo.displayInfo();
+            console.log('----------------------');
+        });
+    }
+}
+
+const todoList = new TodoList();
+
+const todo1 = new TodoItem('Einkaufen', 'Milch, Brot, Eier kaufen', '2022-12-31', 'High');
+const todo2 = new TodoItem('Sport machen', 'Eine Stunde joggen im Park', '2022-12-30', 'Medium');
+
+todoList.addTodo(todo1);
+todoList.addTodo(todo2);
+
+todoList.displayTodos();
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: "/src/index.html",
+        template: "/src/index.html"
     })
   ],
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
