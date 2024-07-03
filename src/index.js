8f6ee3e6ba23416c03c42e22e7f8801317419c38
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-07-03 10:47:16 +0200

    Add functions to edit todo details

--- src/index.js
+++ src/index.js
@@ -38,31 +38,76 @@ class TodoList {
         //});
     }
 
+    findTodoIndex(todoItem) {
+        return this.todos.findIndex(todo => todo === todoItem);
+    }
+
     deleteTodoItem(todoItem) {
-        const index = this.todos.findIndex(todo => todo === todoItem);
+        const index = this.findTodoIndex(todoItem);
         if (index !== -1) {
             this.todos.splice(index, 1);
-            console.log('Todo got deleted.');
+            console.log('Todo deleted.');
         } else {
-            error.log("Couldn't find Todo.");
+            console.error("Can't find Todo.");
+            
         }
 
     }
 
     markTodoItemFinished(todoItem) {
-        const index = this.todos.findIndex(todo => todo === todoItem);
+        const index = this.findTodoIndex(todoItem);
         const finishedTodo = this.todos[index];
         if (index !== -1) {
             this.todos.splice(index, 1);
             this.finishedTodos.push(finishedTodo);
-            console.log('Todo got deleted.');
+            console.log('Todo marked as finished.');
         } else {
-            error.log("Couldn't find Todo.");
+            console.error("Can't find Todo.");
         }
     }
 
     displayFinishedTodos () {
         console.log(this.finishedTodos);
+    }
+
+    editTodoItemTitle(todoItem, newTitle) {
+        const index = this.findTodoIndex(todoItem);
+        if (index !== -1) {
+            this.todos[index].title = newTitle;
+            console.log('Todo title updated successfull');
+        } else {
+            console.error("Can't find Todo.");
+        }
+    }
+
+    editTodoItemDescription(todoItem, newDescription) {
+        const index = this.findTodoIndex(todoItem);
+        if (index !== -1) {
+            this.todos[index].description = newDescription;
+            console.log('Todo description updated successfully.');
+        } else {
+            console.error("Can't find Todo.");
+        }
+    }
+
+    editTodoItemDueDate(todoItem, newDueDate) {
+        const index = this.findTodoIndex(todoItem);
+        if (index !== -1) {
+            this.todos[index].dueDate = newDueDate;
+            console.log('Todo due date updated successfully.');
+        } else {
+            console.error("Can't find Todo.");
+        }
+    }
+
+    editTodoItemPriority(todoItem, newPriority) {
+        const index = this.findTodoIndex(todoItem);
+        if (index !== -1) {
+            this.todos[index].priority = newPriority;
+            console.log('Todo priority updated successfully.');
+        } else {
+            console.error("Can't find Todo.");
+        }
     }
 }
 
@@ -100,5 +145,14 @@ secondTodoList.deleteTodoItem(todo6);
 secondTodoList.displayTodos();
 
 firstTodoList.markTodoItemFinished(todo3);
+firstTodoList.markTodoItemFinished(todo8);
 firstTodoList.displayTodos();
 firstTodoList.displayFinishedTodos();
+
+
+firstTodoList.editTodoItemTitle(todo4, "Complete Project Presentation");
+firstTodoList.editTodoItemDescription(todo4, "Prepare slides and practice speech");
+firstTodoList.editTodoItemDueDate(todo4, "2022-12-15");
+firstTodoList.editTodoItemPriority(todo4, "Low");
+firstTodoList.displayTodos();
+
